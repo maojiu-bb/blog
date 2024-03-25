7b15d61c3f09fac7a5b43c49a33ef2cbcad441c0
maojiu <maojiu-bb@outlook.com>
2024-03-25 18:16:35 +0800

    feat -> 添加首页内容

--- lib/constants/images.dart
+++ lib/constants/images.dart
@@ -1,4 +1,6 @@
 class AssetsImages {
   static const avatar = 'images/avatar.jpeg';
   static const logo = 'images/logo.png';
+  static const cat = 'images/cat.png';
+  static const codeCat = 'images/cat-code.png';
 }
--- lib/pages/home/view.dart
+++ lib/pages/home/view.dart
@@ -5,10 +5,60 @@ import 'package:get/get.dart';
 class HomePage extends GetView<HomeController> {
   const HomePage({super.key});
 
+  Widget _buildMain() {
+    return SliverToBoxAdapter(
+      child: Column(
+        children: [
+          Image.asset(
+            AssetsImages.logo,
+            width: 300,
+            height: 300,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                '会写代码的猫',
+                style: TextStyle(
+                  color: AppColors.primary,
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Image.asset(
+                AssetsImages.cat,
+                width: 100,
+                height: 100,
+              ),
+            ],
+          ),
+          Text(
+            '在午夜时分相遇，寻找真实的自己',
+            style: TextStyle(
+              color: AppColors.onBackground,
+              fontSize: 20,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(50),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(15),
+              child: Image.asset(
+                AssetsImages.codeCat,
+                width: 700,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   Widget _buildView() {
     return CustomScrollView(
       slivers: [
         sliverAppBar(),
+        _buildMain(),
       ],
     );
   }
